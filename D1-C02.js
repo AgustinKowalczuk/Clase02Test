e1a0d430df26ee08b75b3701876e06f952db8714
Agustin Kowalczuk <75805313+AgustinKowalczuk@users.noreply.github.com>
2023-02-26 10:18:22 -0300

    firstComit

---
+++ D1-C02.js
@@ -0,0 +1,52 @@
+//Ejercicio 01 clase 02
+//↓↓↓ Les dejo las consignas para que puedan utilizarlas sin salir de VSCode. ↓↓↓
+/* 
+    -Generar una clase llamada ProductManager (PM)
+    -La clase será un arreglo con Obj de los productos
+    -Debe implementar los siguientes metodos:
+        + addProduct(): Generar nuevos productos y cargarlos en el arreglo.
+            # Si el Nuevo producto tiene el mismo "code" que alguno de los ya cargados: 
+              $verdadero: No se carga el producto y retorna un "Not Found".
+              $falso: Continua con la ejecucion normal. 
+        + getProduct(): Mostrar todos los productos del Arreglo.
+        + getById(_id): Dos caminos Verdadero y Falso.
+            $verdadero : Mostrar el producto.
+            $Falso : Mostrar el msj "Not Found".
+    -Propiedades del Product:
+        + title       : Titulo del producto
+        + description : Descripcion del producto
+        + price       : precio del producto
+        + thumbnail   : imagen del producto (no es obligatoria la carga)
+        + code        : codigo del producto (Este no debe ser repetido)
+        + stock       : cantidad disponible
+        + id          : identificador unico (no es necesario que sea random)
+    -ejemplo de producto: (Pueden utilizarlo para gargar sus productos)
+    {
+        title       :"Celular x120",
+        description :"Celular x120 celular de alta gama... etc,etc.",
+        price       :120800,
+        thumbnail   :"Url_de_la_Img",
+        code        :"Cx120AG",
+        stock       :200,
+        id          :0
+    }
+*/
+
+class ProductManager {
+/*
+    constructor() {}
+    
+    //Debe Agregar Productos con un Id Automatico
+    addProduct() {}
+    
+    //Muestra El array de todos los productos
+    getProducts() {}
+    
+    //Muestra el producto con busqueda por id o retorna un `Not Found`
+    getProductById() {}
+*/
+}
+
+
+//↓↓↓ PROHIBIDO TOCAR ↓↓↓
+module.exports = ProductManager;
